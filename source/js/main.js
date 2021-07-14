f2abecf9afa054b943223f73d800fd9353aeaca1
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-07-14 18:48:10 +0300

    начала верстки главной страницы

---
+++ source/js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var questions = document.querySelector('.questions');
+var accordionHeading = questions.querySelectorAll('h3');
+
+if (accordionHeading) {
+  accordionHeading.forEach(function (item) {
+    item.addEventListener('click', function () {
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+      } else {
+        accordionHeading.forEach(function (element) {
+          if (element.classList.contains('active')) {
+            element.classList.remove('active');
+          }
+        });
+        item.classList.add('active');
+      }
+    });
+  });
+}
