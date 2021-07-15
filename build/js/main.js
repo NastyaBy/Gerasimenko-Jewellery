242a95b31a8048312c79dd1d7dc78c8da4d0756f
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-07-16 00:03:06 +0300

    начала верстки главной страницы,login

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,7 @@
+'use strict';
+
 var questions = document.querySelector('.questions');
 var accordionHeading = questions.querySelectorAll('h3');
-
 
 if (accordionHeading) {
   accordionHeading.forEach(function (item) {
@@ -15,6 +16,54 @@ if (accordionHeading) {
         });
         item.classList.add('active');
       }
+    });
+  });
+}
+
+var layout = document.querySelector('.layout');
+var modalTriggers = document.querySelectorAll('.js-modalOpen');
+
+if (modalTriggers) {
+  modalTriggers.forEach(function (trigger) {
+    trigger.addEventListener('click', function (evt) {
+      var popupTrigger = trigger.dataset.popupTrigger;
+      var popupModal = document.querySelector('[data-popup-modal=' + popupTrigger + ']');
+      var popupFocusInput = trigger.dataset.popupFocusInput;
+      var popupFocus = popupModal.querySelector(popupFocusInput);
+      var bodyBlackout = popupModal.querySelector('.js-modalBlackout');
+      var modalCloseBtn = popupModal.querySelector('.js-modalCloseBtn');
+
+      if (popupFocus) {
+        popupFocus.focus();
+      }
+
+      evt.preventDefault();
+      popupModal.classList.add('is--visible');
+      bodyBlackout.classList.add('is-blacked-out');
+      layout.classList.add('layout--no-scroll');
+
+      bodyBlackout.addEventListener('click', function () {
+        popupModal.classList.remove('is--visible');
+        bodyBlackout.classList.remove('is-blacked-out');
+        layout.classList.remove('layout--no-scroll');
+      });
+
+      modalCloseBtn.addEventListener('click', function () {
+        popupModal.classList.remove('is--visible');
+        bodyBlackout.classList.remove('is-blacked-out');
+        layout.classList.remove('layout--no-scroll');
+      });
+
+      window.addEventListener('keydown', function (e) {
+        if (e.keyCode === 27) {
+          if (popupModal.classList.contains('is--visible')) {
+            e.preventDefault();
+            popupModal.classList.remove('is--visible');
+            bodyBlackout.classList.remove('is-blacked-out');
+            layout.classList.remove('layout--no-scroll');
+          }
+        }
+      });
     });
   });
 }
--- source/js/main.js
+++ source/js/main.js
@@ -19,3 +19,51 @@ if (accordionHeading) {
     });
   });
 }
+
+var layout = document.querySelector('.layout');
+var modalTriggers = document.querySelectorAll('.js-modalOpen');
+
+if (modalTriggers) {
+  modalTriggers.forEach(function (trigger) {
+    trigger.addEventListener('click', function (evt) {
+      var popupTrigger = trigger.dataset.popupTrigger;
+      var popupModal = document.querySelector('[data-popup-modal=' + popupTrigger + ']');
+      var popupFocusInput = trigger.dataset.popupFocusInput;
+      var popupFocus = popupModal.querySelector(popupFocusInput);
+      var bodyBlackout = popupModal.querySelector('.js-modalBlackout');
+      var modalCloseBtn = popupModal.querySelector('.js-modalCloseBtn');
+
+      if (popupFocus) {
+        popupFocus.focus();
+      }
+
+      evt.preventDefault();
+      popupModal.classList.add('is--visible');
+      bodyBlackout.classList.add('is-blacked-out');
+      layout.classList.add('layout--no-scroll');
+
+      bodyBlackout.addEventListener('click', function () {
+        popupModal.classList.remove('is--visible');
+        bodyBlackout.classList.remove('is-blacked-out');
+        layout.classList.remove('layout--no-scroll');
+      });
+
+      modalCloseBtn.addEventListener('click', function () {
+        popupModal.classList.remove('is--visible');
+        bodyBlackout.classList.remove('is-blacked-out');
+        layout.classList.remove('layout--no-scroll');
+      });
+
+      window.addEventListener('keydown', function (e) {
+        if (e.keyCode === 27) {
+          if (popupModal.classList.contains('is--visible')) {
+            e.preventDefault();
+            popupModal.classList.remove('is--visible');
+            bodyBlackout.classList.remove('is-blacked-out');
+            layout.classList.remove('layout--no-scroll');
+          }
+        }
+      });
+    });
+  });
+}
