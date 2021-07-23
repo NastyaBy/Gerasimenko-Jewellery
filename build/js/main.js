aeda5885cd189854d6faab9e015e34811345ae65
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-07-23 16:56:11 +0300

    фиксы

--- build/js/main.js
+++ build/js/main.js
@@ -1,149 +1,169 @@
 'use strict';
 
-var layout = document.querySelector('.layout');
-
-var noJsIndicator = document.querySelector('html.no-js');
-if (noJsIndicator) {
-  noJsIndicator.classList.remove('no-js');
-}
-
-var menuToggleBtn = document.querySelector('.js-menuToggle');
-if (menuToggleBtn) {
-  var layoutHeader = document.querySelector('.layout__header');
-
-  menuToggleBtn.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    layout.classList.toggle('layout--is-menu');
-    layoutHeader.classList.toggle('layout__header--is-menu');
-  });
-}
-
-var questions = document.querySelector('.js-questions');
-if (questions) {
-
-  var activeQuestions = questions.querySelectorAll('.active');
-  if (activeQuestions) {
-    activeQuestions.forEach(function (item) {
-      item.classList.remove('active');
-    });
+(function () {
+  var noJsIndicator = document.querySelector('html.no-js');
+  if (noJsIndicator) {
+    noJsIndicator.classList.remove('no-js');
   }
+})();
 
-  var accordionHeading = questions.querySelectorAll('h3');
-  if (accordionHeading) {
-    accordionHeading.forEach(function (item) {
-      item.addEventListener('click', function () {
-        if (item.classList.contains('active')) {
-          item.classList.remove('active');
-        } else {
-          accordionHeading.forEach(function (element) {
-            if (element.classList.contains('active')) {
-              element.classList.remove('active');
-            }
-          });
-          item.classList.add('active');
-        }
-      });
-    });
-  }
-}
+(function () {
+  var layout = document.querySelector('.layout');
+  var menuToggleBtn = document.querySelector('.js-menuToggle');
+  if (menuToggleBtn) {
+    var layoutHeader = document.querySelector('.layout__header');
 
-var activeFilter = document.querySelectorAll('.active-filter');
-if (activeFilter) {
-  activeFilter.forEach(function (item) {
-    item.classList.remove('active-filter');
-  });
-}
-
-var filter = document.querySelector('.filter');
-if (filter) {
-  var accordionFilter = filter.querySelectorAll('h3');
-
-  if (accordionFilter) {
-    accordionFilter.forEach(function (item) {
-      item.addEventListener('click', function () {
-        item.classList.toggle('active-filter');
-      });
-    });
-  }
-}
-
-var sendForms = document.querySelectorAll('.js-sendForm');
-if (sendForms) {
-  sendForms.forEach(function (trigger) {
-    trigger.addEventListener('click', () => {
-      var form = trigger.closest("form");
-      var inputEmail = form.querySelector('.js-email');
-      var inputPassword = form.querySelector('.js-password');
-
-      if (inputEmail && inputPassword) {
-        var obj = {
-          name: inputEmail.value,
-          tel: inputPassword.value,
-        };
-
-        var serialObj = JSON.stringify(obj);
-        localStorage.setItem('order', serialObj);
-      }
-    });
-  });
-}
-
-var modalTriggers = document.querySelectorAll('.js-modalOpen');
-if (modalTriggers) {
-  modalTriggers.forEach(function (trigger) {
-    trigger.addEventListener('click', function (evt) {
-      var popupTrigger = trigger.dataset.popupTrigger;
-      var popupModal = document.querySelector(
-        '[data-popup-modal=' + popupTrigger + ']');
-      var popupFocusInput = trigger.dataset.popupFocusInput;
-      var popupFocus = popupModal.querySelector(popupFocusInput);
-      var bodyBlackout = popupModal.querySelector('.js-modalBlackout');
-      var modalCloseBtn = popupModal.querySelector('.js-modalCloseBtn');
-      var popupScrollable = trigger.dataset.popupScrollable;
-
-      if (popupFocus) {
-        popupFocus.focus();
-      }
-
+    menuToggleBtn.addEventListener('click', function (evt) {
       evt.preventDefault();
-      popupModal.classList.add('is--visible');
-      if(bodyBlackout) {
-        bodyBlackout.classList.add('is-blacked-out');
+      layout.classList.toggle('layout--is-menu');
+      layoutHeader.classList.toggle('layout__header--is-menu');
+    });
+  }
+})();
 
-        bodyBlackout.addEventListener('click', function () {
-          popupModal.classList.remove('is--visible');
-          bodyBlackout.classList.remove('is-blacked-out');
-          layout.classList.remove('layout--no-scroll');
-        });
-      }
-      if(!popupScrollable) {
-        layout.classList.add('layout--no-scroll');
-      }
+(function () {
+  var questions = document.querySelector('.js-questions');
 
-      modalCloseBtn.addEventListener('click', function () {
-        popupModal.classList.remove('is--visible');
-        if(bodyBlackout) {
-          bodyBlackout.classList.remove('is-blacked-out');
-        }
-        if(!popupScrollable) {
-          layout.classList.remove('layout--no-scroll');
+  var togglerHandleClick = function (evt) {
+    var item = evt.target;
+
+    if (item.classList.contains('active')) {
+      item.classList.remove('active');
+    } else {
+      accordionHeading.forEach(function (element) {
+        if (element.classList.contains('active')) {
+          element.classList.remove('active');
         }
       });
+      item.classList.add('active');
+    }
+  }
 
-      window.addEventListener('keydown', function(e) {
-        if (e.keyCode === 27) {
-          if (popupModal.classList.contains('is--visible')) {
-            e.preventDefault();
-            popupModal.classList.remove('is--visible');
-            if(bodyBlackout) {
-              bodyBlackout.classList.remove('is-blacked-out');
-            }
-            if(!popupScrollable) {
-              layout.classList.remove('layout--no-scroll');
-            }
+  if (questions) {
+    var activeQuestions = questions.querySelectorAll('.active');
+    if (activeQuestions) {
+      activeQuestions.forEach(function (item) {
+        item.classList.remove('active');
+      });
+    }
+
+    var accordionHeading = questions.querySelectorAll('h3');
+    if (accordionHeading) {
+      accordionHeading.forEach(function (item) {
+        item.addEventListener('click', togglerHandleClick);
+      });
+    }
+  }
+})();
+
+(function () {
+  var activeFilter = document.querySelectorAll('.active-filter');
+  if (activeFilter) {
+    activeFilter.forEach(function (item) {
+      item.classList.remove('active-filter');
+    });
+  }
+})();
+
+(function () {
+  var filter = document.querySelector('.filter');
+  if (filter) {
+    var accordionFilter = filter.querySelectorAll('h3');
+
+    if (accordionFilter) {
+      accordionFilter.forEach(function (item) {
+        item.addEventListener('click', function () {
+          item.classList.toggle('active-filter');
+        });
+      });
+    }
+  }
+})();
+
+(function () {
+  var sendForms = document.querySelectorAll('.js-sendForm');
+  if (sendForms) {
+    sendForms.forEach(function (trigger) {
+      trigger.addEventListener('click', function () {
+        var form = trigger.closest('form');
+        var inputEmail = form.querySelector('.js-email');
+        var inputPassword = form.querySelector('.js-password');
+
+        if (inputEmail && inputPassword) {
+          var obj = {
+            name: inputEmail.value,
+            tel: inputPassword.value,
+          };
+
+          var serialObj = JSON.stringify(obj);
+          localStorage.setItem('order', serialObj);
+        }
+      });
+    });
+  }
+})();
+
+(function () {
+  var layout = document.querySelector('.layout');
+  var modalTriggers = document.querySelectorAll('.js-modalOpen');
+
+  var triggerHandleClick = function (trigger, evt) {
+    var popupTrigger = trigger.dataset.popupTrigger;
+    var popupModal = document.querySelector('[data-popup-modal=' + popupTrigger + ']');
+    var popupFocusInput = trigger.dataset.popupFocusInput;
+    var popupFocus = popupModal.querySelector(popupFocusInput);
+    var bodyBlackout = popupModal.querySelector('.js-modalBlackout');
+    var modalCloseBtn = popupModal.querySelector('.js-modalCloseBtn');
+    var popupScrollable = trigger.dataset.popupScrollable;
+
+    if (popupFocus) {
+      popupFocus.focus();
+    }
+
+    evt.preventDefault();
+    popupModal.classList.add('is--visible');
+    if (bodyBlackout) {
+      bodyBlackout.classList.add('is-blacked-out');
+
+      bodyBlackout.addEventListener('click', function () {
+        popupModal.classList.remove('is--visible');
+        bodyBlackout.classList.remove('is-blacked-out');
+        layout.classList.remove('layout--no-scroll');
+      });
+    }
+    if (!popupScrollable) {
+      layout.classList.add('layout--no-scroll');
+    }
+
+    modalCloseBtn.addEventListener('click', function () {
+      popupModal.classList.remove('is--visible');
+      if (bodyBlackout) {
+        bodyBlackout.classList.remove('is-blacked-out');
+      }
+      if (!popupScrollable) {
+        layout.classList.remove('layout--no-scroll');
+      }
+    });
+
+    window.addEventListener('keydown', function (e) {
+      if (e.keyCode === 27) {
+        if (popupModal.classList.contains('is--visible')) {
+          e.preventDefault();
+          popupModal.classList.remove('is--visible');
+          if (bodyBlackout) {
+            bodyBlackout.classList.remove('is-blacked-out');
+          }
+          if (!popupScrollable) {
+            layout.classList.remove('layout--no-scroll');
           }
         }
-      });
+      }
     });
-  });
-}
+  }
+
+  if (modalTriggers) {
+    modalTriggers.forEach(function (trigger) {
+      trigger.addEventListener('click', triggerHandleClick.bind(null, trigger), false);
+    });
+  }
+})();
